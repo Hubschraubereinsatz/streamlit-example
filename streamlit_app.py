47c4a546faf4cb389f31083623f5f327f050c5a7
Benno.Koesters <beko00002@stud.uni-saarland.de>
2023-10-27 18:37:57 +0200

    name

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 from datetime import datetime, timedelta
 from PIL import Image
+import pydeck as pdk
 
 st.write("# CanopyAI 🌳👑")
 
@@ -38,18 +39,41 @@ with tab1:
     with st.sidebar:
         st.button("Reset", type="primary")
         if st.button('centralize '):
+            pdk.View(
+                longitude=8.65027, latitude=49.87167 , zoom=6, min_zoom=5, max_zoom=15, pitch=0, bearing=0
+            )
             st.write('centralized to darmstadt')
         else:
             st.write('fuck off')
 
 with tab2:
-    df = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-        columns=['lat', 'lon'])
-
-    st.map(df, longitude=8.65027, latitude=49.87167)
-
-
+    UK_ACCIDENTS_DATA = (
+        "https://raw.githubusercontent.com/uber-common/"
+        "deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"
+    )
+    layer = pdk.Layer(
+        "HexagonLayer",
+        UK_ACCIDENTS_DATA,
+        get_position="[lng, lat]",
+        auto_highlight=True,
+        elevation_scale=50,
+        pickable=True,
+        elevation_range=[0, 3000],
+        extruded=True,
+        coverage=1,
+    )
+    # Set the viewport location
+    view_state = pdk.ViewState(
+        longitude=8.65027, latitude=49.87167 , zoom=6, min_zoom=5, max_zoom=15, pitch=0, bearing=0
+    )
+    # Combined all of it and render a viewport
+    r = pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        layers=[layer],
+        initial_view_state=view_state,
+        tooltip={"html": "<b>Elevation Value:</b> {elevationValue}", "style": {"color": "white"}},
+    )
+    st.pydeck_chart(r)
 
 with tab3:
 
